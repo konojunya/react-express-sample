c80ed9b1210d1f495c4b8eb9d1d61c976c333275
konojunya <junya03dance@gmail.com>
2018-05-26 20:21:18 +0900

    コメントを追加

--- server.js
+++ server.js
@@ -2,15 +2,14 @@ const express = require("express")
 const bodyParser = require("body-parser")
 const app = express()
 
-app.use(express.static("public"))
-app.use(bodyParser.json())
+app.use(express.static("public")) // publicをstaticにする
+app.use(bodyParser.json()) // postのpayloadを受け取るための設定
 
 app.get("/", (req, res) => {
-  res.sendfile("view/index.html")
+  res.sendfile("view/index.html") // view/index.htmlをレンダリング
 })
 
 // api
-
 let products = [
   { name: "A本", price: 3000 },
   { name: "B本", price: 1000 },
@@ -18,14 +17,14 @@ let products = [
 ]
 
 app.get("/api/products", (req, res) => {
-  res.json(products)
+  res.json(products) // 全体を返す本来ならDBから取得
 })
 app.post("/api/products", (req, res) => {
   products.push({
     name: req.body.name,
     price: req.body.price
   })
-  res.sendStatus(200)
+  res.sendStatus(201) // 201で返す（201 Created）
 })
 
 app.listen(3000)
